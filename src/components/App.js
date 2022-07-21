53e3ad02a339d6e87a7d994d33623c34ba626609
Andrea <acesparza05@gmail.com>
2022-07-21 16:02:25 -0500

    pair programming

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
-
+// import Pig from "./Pig";
+import PigList from "./PigList"
 import hogs from "../porkers_data";
+import PigFilter from "./PigFilter";
+
 
 function App() {
+	const [showGreased, setShowGreased] = useState(false);
+
 	return (
 		<div className="App">
 			<Nav />
+			<PigFilter handleGreased={setShowGreased} />
+			<PigList hogs={hogs} />
 		</div>
 	);
 }
